81e45c4b75e0ad145aad6dffcc3f8f5f72039655
Zoey6zzx <98821820+Zoey6zzx@users.noreply.github.com>
2022-06-02 15:59:20 +0200

    footer added

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../footer/Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h6>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget
+        vehicula tellus, id vestibulum metus. Donec congue augue ac magna mollis
+        euismod. Donec vestibulum odio sed elit rhoncus commodo.
+      </h6>
+    </footer>
+  );
+};
+
+export default Footer;
