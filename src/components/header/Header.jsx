4039459fb230fb60be366c155977959def6b6d16
Zoey6zzx <98821820+Zoey6zzx@users.noreply.github.com>
2022-06-02 15:52:18 +0200

    header added

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="/aalto_it.png" alt="logo" />
+    </header>
+  );
+};
+
+export default Header;
